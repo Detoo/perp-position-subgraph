a4b778fb993c4c75f1623ddbd10e802eb03c32ab
detoo <daily.detoo@gmail.com>
2020-12-06 17:36:27 +0800

    add basic schemas and mappings

---
+++ src/mappings/amm.ts
@@ -0,0 +1,16 @@
+import {
+  FundingRateUpdated,
+} from "../../generated/AmmBTCUSDC/Amm"
+import {
+  FundingRateUpdatedEvent,
+} from "../../generated/schema"
+
+/* Funding rate/payment event
+ */
+export function handleFundingRateUpdated(event: FundingRateUpdated): void {
+  let entity = new FundingRateUpdatedEvent(`${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`)
+  entity.amm = event.address
+  entity.rate = event.params.rate
+  entity.underlyingPrice = event.params.underlyingPrice
+  entity.save()
+}
---
+++ src/mappings/clearingHouse.ts
@@ -0,0 +1,48 @@
+import {
+  PositionChanged,
+  PositionLiquidated,
+} from "../../generated/ClearingHouse/ClearingHouse"
+import {
+  Position,
+  AmmPosition,
+  PositionChangedEvent,
+  PositionLiquidatedEvent,
+} from "../../generated/schema"
+
+/* Trader open/close/modify position
+ */
+export function handlePositionChanged(event: PositionChanged): void {
+  let entity = new PositionChangedEvent(`${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`)
+  entity.trader = event.params.trader
+  entity.amm = event.params.amm
+  entity.margin = event.params.margin
+  entity.positionNotional = event.params.positionNotional
+  entity.exchangedPositionSize = event.params.exchangedPositionSize
+  entity.fee = event.params.fee
+  entity.positionSizeAfter = event.params.positionSizeAfter
+  entity.realizedPnl = event.params.realizedPnl
+  entity.unrealizedPnlAfter = event.params.unrealizedPnlAfter
+  entity.badDebt = event.params.badDebt
+  entity.liquidationPenalty = event.params.liquidationPenalty
+  entity.quoteAssetReserve = event.params.quoteAssetReserve
+  entity.baseAssetReserve = event.params.baseAssetReserve
+  entity.save()
+}
+
+/* Trader position liquidated
+ *
+ * - always comes after a corresponding PositionChanged event
+ * - the PositionChanged event represents the position changes due to liquidation, and the PositionLiquidated event
+ *   represents the liquidation-specific information
+ */
+export function handlePositionLiquidated(event: PositionLiquidated): void {
+  let entity = new PositionLiquidatedEvent(`${event.transaction.hash.toHexString()}-${event.logIndex.toString()}`)
+  entity.trader = event.params.trader
+  entity.amm = event.params.amm
+  entity.positionNotional = event.params.positionNotional
+  entity.positionSize = event.params.positionSize
+  entity.liquidationFee = event.params.liquidationFee
+  entity.liquidator = event.params.liquidator
+  entity.badDebt = event.params.badDebt
+  entity.save()
+}
